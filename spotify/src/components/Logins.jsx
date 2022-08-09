6b50d7a667642f89948f88f067c8c2e6efef423d
Ruturaj Salunkhe <103729113+Ruturajwork@users.noreply.github.com>
2022-08-10 00:14:39 +0530

    Update Logins.jsx

--- spotify/src/components/Logins.jsx
+++ spotify/src/components/Logins.jsx
@@ -3,62 +3,8 @@ import styled from "styled-components";
 
 export default function Login() {
   const handleClick = async () => {
-    const clientId = "d984dc138a2d48e8af3d750730c92b48";
-    const redirectUrl = "import React from "react";
-import styled from "styled-components";
-
-export default function Login() {
-  const handleClick = async () => {
-    const clientId = "d984dc138a2d48e8af3d750730c92b48";
+    const clientId = "b10ac7cc459d474e961a6603c15da715";
     const redirectUrl = "https://spotifytask.vercel.app/";
-    const apiUrl = "https://accounts.spotify.com/authorize";
-    const scope = [
-      "user-read-email",
-      "user-read-private",
-      "user-modify-playback-state",
-      "user-read-playback-state",
-      "user-read-currently-playing",
-      "user-read-recently-played",
-      "user-read-playback-position",
-      "user-top-read",
-    ];
-    window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(
-      " "
-    )}&response_type=token&show_dialog=true`;
-  };
-  return (
-    <Container>
-      <img
-        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"
-        alt="spotify"
-      />
-      <button onClick={handleClick}>Connect Spotify</button>
-    </Container>
-  );
-}
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  height: 100vh;
-  width: 100vw;
-  background-color: #1db954;
-  gap: 5rem;
-  img {
-    height: 20vh;
-  }
-  button {
-    padding: 1rem 5rem;
-    border-radius: 5rem;
-    background-color: black;
-    color: #49f585;
-    border: none;
-    font-size: 1.4rem;
-    cursor: pointer;
-  }
-`;";
     const apiUrl = "https://accounts.spotify.com/authorize";
     const scope = [
       "user-read-email",
